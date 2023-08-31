82e45c3fffd9bea73b95424a0af083d929f494de
zhouhp1295 <554949297@qq.com>
2023-08-31 14:20:56 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $path
-npm install
+npm install --ignore-scripts
 cp -rf custom/browser-pack/_prelude.js node_modules/browser-pack/_prelude.js
-ln -s $path/layaair2-plus-cmd.js /usr/local/bin/layaair2-plus-cmd
-
-
-
